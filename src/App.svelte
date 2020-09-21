44b4430f08739d71160795feac9557e9eec043cf
Nengwen Zhuo <soarowl@yeah.net>
2020-09-21 20:49:08 +0800

    Format source according using svelte format.

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,23 @@
 <script lang="ts">
-	import ModeSwitcher from './ModeSwitcher.svelte';
-	import Tailwindcss from './Tailwindcss.svelte';
-	export let name: string;
+  import ModeSwitcher from "./ModeSwitcher.svelte";
+  import Tailwindcss from "./Tailwindcss.svelte";
+  export let name: string;
 </script>
+
 <style>
-	.custom-style {
-		@apply italic;
-	}
+  .custom-style {
+    @apply italic;
+  }
 </style>
+
 <Tailwindcss />
 <ModeSwitcher />
 <main class="p-4 mx-auto text-center max-w-xl">
-	<h1 class="uppercase text-6xl font-hairline text-svelte">Hello {name}!</h1>
-	<p class="custom-style">
-		Visit the
-		<a href="https://svelte.dev/tutorial" class="text-blue-500 underline">Svelte tutorial</a>
-		to learn how to build Svelte apps.
-	</p>
+  <h1 class="uppercase text-6xl font-hairline text-svelte">Hello {name}!</h1>
+  <p class="custom-style">
+    Visit the <a
+      href="https://svelte.dev/tutorial"
+      class="text-blue-500 underline">Svelte tutorial</a> to learn how to build Svelte
+    apps.
+  </p>
 </main>
